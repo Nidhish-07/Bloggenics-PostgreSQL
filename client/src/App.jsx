1845fe1d02f6e5a42918f37e595bcbeab63103c0
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-08 21:57:49 +0530

    Routing in client added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import WriteBlog from "./pages/WriteBlog";
+import PostBlog from './pages/PostBlog'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const Layout = () => {
   return (
     <div>
-      {/* <Navbar></Navbar> */}
+      <Navbar></Navbar>
       <Outlet></Outlet>
-      {/* <Footer></Footer> */}
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <div>hello</div>,
-  children: [{
+const router = createBrowserRouter([
+  {
     path: "/",
-    element: <div>asdasdsd</div>
-  }]
-}])
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/write",
+        element: <WriteBlog />,
+      },
+      {
+        path: "/post/:id",
+        element: <PostBlog />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+
+  },
+  {
+    path: "/login",
+    element: <Login />,
+
+  },
+]);
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
