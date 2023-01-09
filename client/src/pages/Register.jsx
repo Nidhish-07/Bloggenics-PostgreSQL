466cd846e009e41ee107e22185ff12359cf6f35e
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-09 15:01:39 +0530

    Register page added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="flex flex-col items-center justify-center h-[100vh] bg-[#96B9D9]">
+    <h1 className="text-5xl text-[#262f41] p-6 font-bold">Register</h1>
+    <form className="flex flex-col p-12 bg-white w-80 gap-5 rounded">
+      <input required type="text" placeholder="Username" className="p-2 border-b-2 border-b-solid border-b-[#435373]" />
+      <input required type="email" placeholder="Email" className="p-2 border-b-2 border-b-solid border-b-[#435373]" />
+      <input required type="password" placeholder="Password" className="p-2 border-b-2 border-b-solid border-b-[#435373]" />
+      <input required type="password" placeholder="Confirm Password" className="p-2 border-b-2 border-b-solid border-b-[#435373]" />
+      <button className="bg-[#435373] text-white font-semibold p-2 rounded cursor-pointer">Register</button>
+      <p className="text-red-500 text-sm text-center">Error</p>
+      <span className="text-center ">Account? <Link to={"/login"} className="text-[#96b9d9] font-semibold">Login</Link></span>
+    </form>
+  </div>
   )
 }
 
